57930cdb7bfc9972db716cec3558c58bb0550237
yeungle <75871729+yeungle@users.noreply.github.com>
2025-06-09 09:07:02 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,17 +19,17 @@
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
 
-rm -rf feeds/luci/themes/luci-theme-argon
+#rm -rf feeds/luci/themes/luci-theme-argon
 
-git clone --depth=1 -b main https://github.com/fw876/helloworld.git package/helloworld
+#git clone --depth=1 -b main https://github.com/fw876/helloworld.git package/helloworld
 
-git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome.git package/luci-app-adguardhome
+#git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome.git package/luci-app-adguardhome
 
-git clone --depth=1 https://github.com/kenzok8/small-package.git kenzok8-packages
-cp -rf kenzok8-packages/ddnsto package/ddnsto
-cp -rf kenzok8-packages/luci-app-ddnsto package/luci-app-ddnsto
+#git clone --depth=1 https://github.com/kenzok8/small-package.git kenzok8-packages
+#cp -rf kenzok8-packages/ddnsto package/ddnsto
+#cp -rf kenzok8-packages/luci-app-ddnsto package/luci-app-ddnsto
 
-git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+#git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a -f
