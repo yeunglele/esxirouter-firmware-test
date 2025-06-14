e5a2f800e9eae344a02711ccfa324a9be2dd1af1
yeungle <75871729+yeungle@users.noreply.github.com>
2025-06-14 16:07:09 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,5 +15,29 @@
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-sed -i '1i src-git moruiris https://github.com/moruiris/openwrt-packages;lede' feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+function mvdir() {
+mv -n `find $1/* -maxdepth 0 -type d` ./
+rm -rf $1
+}
+
+git clone https://github.com/kenzok8/openwrt-packages
+mkdir ./packages_luci-app-smartdns
+mv openwrt-packages/luci-app-smartdns ./packages_luci-app-smartdns/
+mv openwrt-packages/smartdns ./packages_luci-app-smartdns/
+rm -rf openwrt-packages
+
+git clone https://github.com/kenzok8/openwrt-packages
+mkdir ./packages_luci-app-adguardhome
+mv openwrt-packages/luci-app-adguardhome ./packages_luci-app-adguardhome/
+mv openwrt-packages/adguardhome ./packages_luci-app-adguardhome/
+rm -rf openwrt-packages
+
+git clone https://github.com/fw876/helloworld ./packages_luci-app-ssr-plus 
+
+git clone -b https://github.com/jerrykuku/luci-theme-argon.git
+
+rm -rf ./*/.git & rm -rf ./*/.gitattributes & rm -rf ./*/.svn & rm -rf ./*/.github & rm -rf ./*/.gitignore
+rm -rf ./*/*/.git & rm -rf ./*/*/.gitattributes &rm -rf ./*/*/.svn & rm -rf ./*/*/.github & rm -rf ./*/*/.gitignore
+exit 0
